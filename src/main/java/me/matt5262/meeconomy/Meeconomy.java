9548cea11e8da993697c9cc4867c74362885905d
Matti <mattibruun2000@gmail.com>
2025-11-21 22:35:47 +0100

    Initialize Meeconomy plugin with main class and configuration files

---
+++ src/main/java/me/matt5262/meeconomy/Meeconomy.java
@@ -0,0 +1,17 @@
+package me.matt5262.meeconomy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Meeconomy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
